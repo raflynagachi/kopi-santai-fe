691cb2ed6fe5f57a5396745e79cd438c5208b7dd
rafly.nagachi <rafly.nagachi@shopee.com>
2022-08-31 15:30:54 +0700

    feat: delete promotion in admin page

--- src/Components/Admin/PromotionTable.js
+++ src/Components/Admin/PromotionTable.js
@@ -1,8 +1,38 @@
+import { useState } from 'react';
 import format from '../../Utils/Format';
+import { API, helpers } from '../../Utils/API';
+import Toast from '../Toast';
 
 export default function PromotionTable({ promotions }) {
+  const [error, setError] = useState(null);
+  const [showToast, setShowToast] = useState(false);
+  const [showToastDeleteSuccess, setShowToastDeleteSuccess] = useState(false);
+  const token = localStorage.getItem('token');
+
+  const deletePromotion = (promoID) => {
+    const url = `${API.Promotions}/${promoID}`;
+    const requestOpt = helpers.requestOptions(null, 'DELETE', token);
+    fetch(url, requestOpt)
+      .then((res) => res.json())
+      .then((result) => {
+        if (result.statusCode === 200) {
+          setShowToastDeleteSuccess(true);
+          setTimeout(() => { window.location.reload(); }, 1200);
+        } else {
+          setError(result.data);
+          setShowToast(error);
+        }
+      })
+      .catch((err) => {
+        setError(err);
+        setShowToast(true);
+      });
+  };
+
   return (
     <div className="container">
+      {error && <Toast show={showToast} setShow={setShowToast} message={error.message} />}
+      <Toast show={showToastDeleteSuccess} setShow={setShowToastDeleteSuccess} message="Promotion deleted successfully" />
       <table className="table table-bordered">
         <thead className="text-center bg-info">
           <tr>
@@ -13,7 +43,7 @@ export default function PromotionTable({ promotions }) {
             <td>Min Spent</td>
             <td>Coupon</td>
             <td>Discount Amount</td>
-            <td>Is Available</td>
+            <td>Coupon Status</td>
             <td>Action</td>
           </tr>
         </thead>
@@ -36,8 +66,8 @@ export default function PromotionTable({ promotions }) {
                 }
                   <td>
                     {
-                      !item.coupon.deletedAt
-                      && <button type="button" className="btn mx-1 p-1" style={{ backgroundColor: '#ff6e6e', width: '6em' }}>Delete</button>
+                      !item.deletedAt
+                      && <button type="button" className="btn mx-1 p-1" style={{ backgroundColor: '#ff6e6e', width: '6em' }} onClick={() => { deletePromotion(item.id); }}>Delete</button>
                     }
                   </td>
                 </tr>
--- src/Components/Games/Gameplay.js
+++ src/Components/Games/Gameplay.js
@@ -1,4 +1,12 @@
+import useFetch from '../../Hooks/useFetch';
+
 export default function Gameplay() {
+  const url = 'https://the-trivia-api.com/api/questions?limit=1&difficulty=easy';
+
+  console.log(url);
+  const quiz = useFetch(url);
+  console.log(quiz);
+
   return (
     <>
       Game
