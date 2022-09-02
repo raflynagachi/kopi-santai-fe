f02e13bec837a88ad2fad0c5eec3de529bffebbd
rafly.nagachi <rafly.nagachi@shopee.com>
2022-09-02 14:01:01 +0700

    refactor: review form

--- src/Components/Review/ReviewForm.js
+++ src/Components/Review/ReviewForm.js
@@ -12,8 +12,17 @@ export default function ReviewForm({ menuItem, handleSubmit }) {
         <br />
         <div className="form-group">
           Rating
-          <div className="d-flex flex-row align-items-center">
-            <input defaultValue={5} type="number" step={0.01} className="form-control" id="rating" min={1} max={5} required />
+          <div className="d-flex flex-row align-items-center form-check">
+            <input type="radio" name="rating" className="form-check-input ms-4 me-1" value={1} />
+            1
+            <input type="radio" name="rating" className="form-check-input ms-4 me-1" value={2} />
+            2
+            <input type="radio" name="rating" className="form-check-input ms-4 me-1" value={3} />
+            3
+            <input type="radio" name="rating" className="form-check-input ms-4 me-1" value={4} />
+            4
+            <input type="radio" name="rating" className="form-check-input ms-4 me-1" value={5} />
+            5
           </div>
         </div>
         <br />
