2ac16bddec609461d64f272f6781602186ed00aa
rafly.nagachi <rafly.nagachi@shopee.com>
2022-09-03 20:01:04 +0700

    fix: display menu card

--- src/Components/Menu/MenuCard.js
+++ src/Components/Menu/MenuCard.js
@@ -7,17 +7,17 @@ export default function MenuCard({ menuItem }) {
       <img className="card-img-top" style={{ height: '14rem' }} src={format.displayByteImage(menuItem.image)} alt="Card menu" />
       <div className="card-body">
         <h5 style={{ fontSize: '0.9em' }} className="card-title">{menuItem.name}</h5>
-        <p style={{ fontSize: '0.75em' }} className="col-6">
+        <p style={{ fontSize: '0.75em' }} className="col-8">
           Category:
           {menuItem.categoryName}
         </p>
         <div className="card-text row">
-          <div className="col-3">
+          <div className="col-5">
             <span className="fa fa-star checked" style={{ color: '#FFD27D' }} />
             {' '}
             {menuItem.rating}
           </div>
-          <div className="col-9 text-end">
+          <div className="col-7 text-end">
             {` ${format.priceFormatter(menuItem.price)}`}
           </div>
         </div>
